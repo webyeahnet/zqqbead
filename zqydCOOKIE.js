1fa1d4ee7c8e837edc59f0e147cde2ad23d58120
webyeahnet <67409453+webyeahnet@users.noreply.github.com>
2021-12-31 18:34:45 +0800

    Update zqydCOOKIE.js

--- zqydCOOKIE.js
+++ zqydCOOKIE.js
@@ -1,7 +1,7 @@
 module.exports = {
     "id": "zqyd",
     "name": "钟情阅读",
-    "keys": ["zqydbody","zqydbody2","zqydbody3","zqydbody4","zqydbody5","zqydbody6"],
+    "keys": ["zqydbody","zqydbody2","zqydbody3","zqydbody4","zqydbody5","zqydbody6","zqydbody7",],
     "author": "@tom",
     "settings": [{
       "id": "zqydSuffix",
@@ -12,7 +12,7 @@ module.exports = {
     }, {
       "id": "zqydCount",
       "name": "账号个数",
-      "val": "3",
+      "val": process.env.zqydCount,
       "type": "number",
       "desc": "指定任务最多跑几个账号，根据抓取的账号数据个数来设值"
     }, {
@@ -57,25 +57,30 @@ module.exports = {
     "script": "https://gitee.com/tom210120/tom/raw/master/ZQ/zq.js",
     "icon": "https://gitee.com/tom210120/tom/raw/master/Tom/Tom.jpg",
     "favIcon": "mdi-star-outline",
-    "favIconColor": "grey", 
+    "favIconColor": "grey",
     "datas": [{
       "key": "zqydbody",
-      "val": "p=PCWwRj3eGxCw%3Dr_eNlWKYuMeKCD0GRCZiId7C1AVNRQUdYBKApsKgatYkYc9QImJlhgGLPxLVXDr1Nz2S4_Gz7eHWm6qLwUns1Y0x5Q3lz6C5cHGebiy1ohYKfxlXoOWLXY8A7vSqJxCnIQtH-eZ-8v4Cgw4Iat5T01IvyQW6NE5OooXjRSOFHI7OkzoE_m3D4gjyTHW6YRi_9Op4DvWnXylRMLTZCiyyumj61YqICbacbEuSjoSXRWjX4ilYRYHUJKoPFrAY2VzUOWWQExRGb_fsJZ41Zs0sV2TIKIZe4-fj2WKgB0r10nfVTqmGUhkKx720eqlxhHIr7tgyv3DqJnmDMUtpRx461mt9i6gjYt54tTzeb-Hkzh3dhND7lQPQxKv63X1HJsqOrtrdhMCXxDAX3b0kPBbDDAWZYIQ_lOsQTF1ryZRqhJHW5Lsq6hi4tAwj-3a4tpKEmKQxNIeUGRbhgKp9bj6Ol3bWDstFb1dZTALBYUq8FjRnzquJBoqRfLxS_VXpjahU8kcQVJii6V9R-Tvx-URgf2oFhlW7PgZmd_0kQb52FTMqo-NifCVvrdHNUwiHRf_mxd6XBlVFsnu7ZDkuxpobDtmc_u8hyuW3HZTeQ92a-nwiOmXwcBahdTeGAnoMvazPjKxDuf16hIQ_vepL1uUHyGHQBNc6wLRQwIKJtqPXhLkrglDN2nc2DC9N2-DX67ccZwYl_PH583a7w9iHac-m5QI4FMOUW-bOnv_nTUo9O2GbYece4dc-Hp28DJ7iUW_0BAnp_VG7lI4KI9cQbH2vn2DzFSZG62-vG7HEQhWd9clcNMvFuizdnbOgNjRE5jL7PajPt_JsBplLyuwoR_97NFZlwfhEq8l6DD14ru7rZih_V3tbYifyt38Xzqrn8aR9KE3mm6H99kwfFwMDCLl6E8wXoTViJtBeIxjoOkezel6Vvq8Pelee-2fZzrdPLXZFztgcGHhsAFOHdfXFDsNkzGJ3OPZQ7WgOJfY8ehDupEjs7KxNnfVJNH55YP7-iYz4lR0VtqfCJPaogkl3l6znYe4L-hRWDIAeIbwuYSUIdFMpFY9kswHMshLG82Hy37Hb2fo7Jic7PZ7OJTLNye3u3fxkuaPQREfOawddEPDLqRF2xYyws1L5R7ZX-1T1HjA4" //1-1
+      "val": process.env.zqydbody
     }, {
       "key": "zqydbody2",
-      "val": "p=PCWwRj3eGxCw%3Dr_eNlWKYuMeKCD0GRCZiId7C1AVNRQUdYBKApsKgatYkYc9QImJlhgGLPxLVXDr1Nz2S4_Gz7eHWm6qLwUns1Y0x5Q3lz6C5cHGebiy1ohYKfxlXoOWLXY8A7vSqJxCnIQtH-eZ-8v4Cgw4Iat5T01IvyQW6NE5OooXjRSOFHI7OkzoE_m3D4gjyTHW6YRi_9Op4DvWnXylRMLTZCiyyumj61YqICbacfaBzM03TquI1j1TDmqaJOFx6vtg40lCtYDpbTtwdm8ulpikCHCFl1h8_UZx5EEEjcE3aAk2librejC5-9tc99b6hYrQU02O3SYN7_c5JHERrGN-QXvZqGEJZuUUqRGRsvc8eqyHPgMpq7rdZmb2nIIhL1lUTt25cPerwPV9V5P3VEDE-ELUWPshu9snRshkEPq27CMVuMc6KkXXf0x4uCKshLxBBt-oSF0w3bnefWCJBphppf2zSawdCPEbkVIKS4ARP4v0uqmQxxa9Unv846NLeIl046W5YEZQcbtsbTWKmanHENZrqXShKr4MLnu2LLYRmYodTf0672Mt2tvKiTEBG3dn2HTmoft43IyY3EPSCRZe4whINXixyUM0h7hFcpFKMjbED41_8Djd1drtQ1ufPofu85BYoMipytuRvxJP4uo2G6dyOx8bovrx-IQ63MM7B6HwOv8CJQqCn3RmSXOoSvMPH_OZPHIx03WeyyK8uDvuOrBstakPRerhLHxNbSW3Jz-o9ZW4G7HrEhgcjYOaZDjQVuFBejSjRvpnB2vaF4OrEOn3m0ZcE89coE9qf-3c05g2qAfgf2Kcrd6iYNqhsR_vn934GLGFsDyWS1g3L2hcAsDD9laEKL-UFBdfMfdqtVYnI3IO7iQIbTI6ysfvj7LcwYA6W9rAYq_7gWGOF2fgxPnHeBNMU-6R3QXLHmdgsRIKr5ydk1p2mZOUQup-GVRf-ivq1XEovRAz33tHCAZSQk654EMm8WIYzaH7Siad7xKL2MB3vl_hj78OYzWIXxM0HvXnabjinDhI1b9adME8kdCyRX-ymOec4NG2zNkGx3QjrCMB4oSH3byYhTQcx8-qXL3IzSXeTsqE9-FFnm1znqyX1x3_X0K9GeCMIMvWYbf0ZXBfFBegeq7aaSln8lTOoom4C" //6-1
+      "val": process.env.zqydbody2
     }, {
       "key": "zqydbody3",
-      "val": "p=jYFg4QJ5A6eY%3DxnUlbsv2ZiAgzg99KCv9uZZnQ5updAM5mdyTGxXXRQXJyTZZzAvvEBrwqVfLXWuJcsEaitasVPkkF4ik8wm-XqV9oebiuy0qPD06hPPvRIBzvWwR2et72gLzVR3hLAHMoM4i6ymJg3p13103QwWMcsyiROV9piG0BneSqWNvD93lyk-3-beOr2KY1w6RHxQflcCDhsvqFyx7omEZgQpPM11fKpdNMTqz6laLoYyxqAMpgk6vC2ppuY7l2xzlwgBXaoWTwETuocKU_JcduOUZGVHdnCd2CbWmA1-SmzmTeSQsgjirJBS47_jXpPvdq6pwR-0PBTi9ubXKviD40lE2MIjYqk-AXSbAf3B6JrqkbFZ-48QFqATMUlD-UofjZi2djGuchATeeEXnActIYWJcy-kRHm76kFtfuwTWZ9BzyBTeJI1eNwN_47Q9o7HqnJFlopFZA5B-wm4F7ADuJXKntpp90QLf4TaE0WHjJkfZrfaQZtB3mKYVlD24GzEpRMlvUEcutcX9CRDlSQ2x_FTbG85qsg-zhmwpCXuhoYgi2gu_7UvjpR-zhVy5J1sHsjFP4_5ue8FX8x2-tjJuwtI-JiFbwUaYxoBVpcyaaehID9bpo3fYIWXov9h3csIMYSAgiuN36OahV6cws6gIo3eorPRZZ34h9HsfSZ64ENj7VipaOtigbyhIjI4S_zLTGkCV3iskkl_Zgbo5fKvSvGlmBwaHfRHRd0aMYEoQBV5NJq493dQwOxg52GBaktBlyu5C4mYvUbBpHUMdBqGAW54FUfc2s6IGUBP75_3njBJ6Hi6adz4QFMZE1HdKjzYAx94bDkj6VuBl3Pjc9YvcCBVmN7yxLLGWPrK3rVnutLy2DNKfmpAZqyRWLPQCXgA-9CwstbU-QzjeupDz0SXGcz1Q3Ru_Bn6d2gaDTiYIyTR5msC5lmug37DSIOBFSmsNTpn6JH8xljOwgxp140KCdsipmsLe9xiw3Q_xC_muq3S2kNcMnhw8026CHYlrusWZgTb0qqzNSzGsak4dHxSWErHXatvufN0aPeQZjwVkY1A8flb2driJ4YDwEumpge9kExgL6cv3shdfvJ74d4Yk2z816hY2T6cMK06GxW4BsyJY20FqWqqA8Bo1uUtnj_TEUFMM" //3-1
+      "val": process.env.zqydbody3
+    }, {
+      "key": "zqydbody4",
+      "val": process.env.zqydbody4
+    }, {
+      "key": "zqydbody5",
+      "val": process.env.zqydbody5
+    }, {
+      "key": "zqydbody6",
+      "val": process.env.zqydbody6
+    }, {
+      "key": "zqydbody7",
+      "val": process.env.zqydbody7
     }],
     "sessions": [],
     "isLoaded": true
   }
-  /*, {
-      "key": "zqydbody4",
-      "val": "" //1-2
-    }*/
-  // %3D
-
-
   
